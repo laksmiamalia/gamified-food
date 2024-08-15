c1647f39cd209950d236c57b7f354d21b3ff2646
Laksmi A Wulandiari <113813929+laksmiamalia@users.noreply.github.com>
2024-08-15 13:51:56 +0700

    Delete cluster_descriptions.py

--- cluster_descriptions.py
+++
@@ -1,8 +0,0 @@
-# cluster_descriptions.py
-
-cluster_descriptions = {
-    0: "Cluster 0 contains games that perform poorly in all regions, possibly indicating niche or less popular genres or platforms, dominated with Action, Sports, and Adventure genre, while Platform genre has the lowest sale. Strategy for this cluster are consider whether these games need reworking, rebranding, or could be targeted to niche audiences better.",
-    1: "Cluster 1 stands out as the cluster with the most successful games globally, indicating these are likely the top-performing games across all regions, dominated with Platform, Role-Playing, and Sports genre, while Simulation genre was the lowest performance out of all genre. Games in this cluster can be used as marketing standard, to help improve performance from the other clusters.",
-    2: "Cluster 2 and Cluster 3 show varying degrees of regional success, with Cluster 3 being moderately successful globally. Both of the cluster are dominated with Action genre. This cluster could be targeted for region-specific marketing strategies, as they show potential in certain regions.",
-    3: "Cluster 2 and Cluster 3 show varying degrees of regional success, with Cluster 3 being moderately successful globally. Both of the cluster are dominated with Action genre. This cluster could be targeted for region-specific marketing strategies, as they show potential in certain regions."
-}
