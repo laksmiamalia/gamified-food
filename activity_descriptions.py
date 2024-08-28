140129b555b69b77e0363c61130e20e2744d5c23
Laksmi A Wulandiari <113813929+laksmiamalia@users.noreply.github.com>
2024-08-28 11:08:45 +0700

    Add files via upload

---
+++ activity_descriptions.py
@@ -0,0 +1,9 @@
+# activity_descriptions.py
+
+activity_descriptions = {
+    Sedentary: "The sedentary activity level includes activities of daily living, without additional exercise. This level includes sitting quietly, riding in a car, walking from the house to the car or bus, household tasks, and light yard work.",
+    Lightly Active: "Lightly Active indicade that you work a job with light physical demands, or you work a desk job and perform light exercise (at the level of a brisk walk) for 30 minutes per day, 3-5 times per week",
+    Moderately Active: "Moderately Active indicade that you work a moderately physically demanding job, such as a construction worker, or you work a desk job and engage in moderate exercise for 1 hour per day, 3-5 times per week.",
+    Very Active: "Very Active indicade that you work a consistently physically demanding job, such as an agricultural worker, or you work a desk job and engage in intense exercise for 1 hour per day or moderate exercise for 2 hours per day, 5-7 times per week.",
+    Super Active: "Super active people tend to have scheduled workouts on most days of the week or intentionally work lengthy walks or runs into their day."
+}
