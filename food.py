2b66b033f16e190346be57575843e1938ed3e186
Laksmi A Wulandiari <113813929+laksmiamalia@users.noreply.github.com>
2024-08-28 11:30:03 +0700

    Update food.py

--- food.py
+++ food.py
@@ -111,7 +111,7 @@ def process_food_data(merged_data, weight_kg, height_cm, age_years, gender, acti
     return merged_data
 
 # Streamlit user form
-st.title('Personalized Nutrition Plan')
+st.set_page_config(page_title="Personalized Nutrition Plan", page_icon="🥗")
 
 st.header('Enter Your Information')
 
